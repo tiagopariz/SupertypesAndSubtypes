30bcd34f82ed02cc6f117157f4dd89829ace936d
tiagopariz <tiagopariz@gmail.com>
2017-12-04 21:52:05 -0200

    Entidades de serviço

--- SupertypesAndSubtypes.Domain/Entities/Service.cs
+++ SupertypesAndSubtypes.Domain/Entities/Service.cs
@@ -4,10 +4,11 @@ namespace SupertypesAndSubtypes.Domain.Entities
 {
     public class Service
     {
-        public Service(Guid serviceId, string serviceName, Guid customerId, DateTime startDate, string customerName, decimal totalTimeSpent)
+        public Service(Guid serviceId, string serviceName, Guid parentServiceId, Guid customerId, DateTime startDate, string customerName, decimal totalTimeSpent)
         {
             ServiceId = serviceId;
             ServiceName = serviceName;
+            ParentServiceId = parentServiceId;
             CustomerId = customerId;
             StartDate = startDate;
             CustomerName = customerName;
@@ -16,6 +17,7 @@ namespace SupertypesAndSubtypes.Domain.Entities
 
         public Guid ServiceId { get; }
         public string ServiceName { get; }
+        public Guid ParentServiceId { get; }
         public Guid CustomerId { get; }
         public DateTime StartDate { get; }
         public string CustomerName { get; }
--- SupertypesAndSubtypes.Prompt/Program.cs
+++ SupertypesAndSubtypes.Prompt/Program.cs
@@ -5,6 +5,7 @@
         private static void Main()
         {
             //https://msdn.microsoft.com/en-us/library/cc505839.aspx
+            //https://books.google.com.br/books?id=HGyraCo-u_EC&pg=PA20&lpg=PA20&dq=c%23+supertype+abstract&source=bl&ots=LhGFEPoHYV&sig=aSd91dqvZ7c7QO0jO_AOPPBlsNE&hl=pt-BR&sa=X&ved=0ahUKEwjo2J22xfHXAhUGgpAKHZUWDwUQ6AEIcTAJ#v=onepage&q=c%23%20supertype%20abstract&f=false
             //Projects(ProjectId, ProjectName, CustomerId, StartDate, CustomerName, TotalTimeSpent)
             //ProjectDetails(ProjectId, ItemId, ActivityId, EmployeeId, WorkDate, TimeSpent)
             //Customers(CustomerId, CustomerName)
